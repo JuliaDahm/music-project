7845f53cb4c4a34ea44fcf81e6261b96b90cae4c
julia dahm <jukabrown@gmail.com>
2019-05-20 15:28:58 -0400

    Build out basic component for handling and rendering data (need to refactor)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
+import axios from 'axios';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+
+export default class App extends React.Component {
+  state = {
+    albums: [],
+    isLoading: true,
+    errors: null
+  };
+
+  componentDidMount() {
+    axios
+      .get(`https://cors-anywhere.herokuapp.com/https://itunes.apple.com/lookup?id=994656&entity=album`)
+      .then(response => 
+        response.data.results.map(album => ({
+          artistName: `${album.artistName}`,
+          albumName: `${album.collectionName}`,
+          artwork: `${album.artworkUrl100}`,
+        }))
+      )
+      .then(albums => {
+        albums.shift()
+        this.setState({
+          albums: albums,
+          isLoading: false
+        });
+      })
+      .catch(error => this.setState({ error, isLoading: false }));
+  }
+
+  render() {
+    const { isLoading, albums } = this.state;
+    return (
+      <React.Fragment>
+        <h2>iTunes</h2>
+        <div>
+          {!isLoading ? (
+            albums.map(album => {
+            const { artistName, albumName, artwork } = album;
+              return (
+                <div key={albumName}>
+                  <p>{artistName}</p>
+                  <div>
+                    <img src={artwork} alt={artistName} />
+                  </div>
+                  <p>{albumName}</p>
+                  <hr />
+                </div>
+              );
+            })
+          ) : (
+            <p>Loading...</p>
+          )}
+        </div>
+      </React.Fragment>
+    );
+  }
+}
