81f9ee39ca9c8f3ad3fa7445d10dfb92703a650b
julia dahm <jukabrown@gmail.com>
2019-05-22 10:07:09 -0400

    Refactor to use unidirectional data flow

--- src/Albums.js
+++ src/Albums.js
@@ -9,63 +9,14 @@ export default class Albums extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      albums: [],
-      isLoading: true,
       showAlbum: false,
-      errors: null, 
       albumToShow: ''
     };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  componentDidMount() {
-    this.fetchData();
-  }
-
-  getComponent() {
-    if (this.state.showAlbum) {
-      return <Album/>;
-    } else {
-      return null;
-    }
-  }
-
-  mapResponse(data) {
-    const albumArr = data.map(album => ({
-      artistName: `${album.artistName}`,
-      albumName: `${album.collectionName}`,
-      artwork: `${album.artworkUrl100}`,
-      albumId: `${album.collectionId}`,
-    }))
-    return albumArr;
-  }
-
-  fetchData(){
-    const url = 'https://cors-anywhere.herokuapp.com/https://itunes.apple.com/lookup?id=994656&entity=album';
-    axios
-      .get(`${url}`)
-      .then(response => 
-        this.mapResponse(response.data.results)
-      )
-      .then(albums => {
-        albums.shift()
-        this.setState({
-          albums: albums,
-          isLoading: false,
-        });
-      })
-      .catch(error => this.setState({ error, isLoading: false }));
-  }
-
-  handleClick(event) {
-    this.setState({
-      showAlbum: !this.state.showAlbum,
-      albumToShow: event.target.id,
-    });
+    // this.handleClick = this.handleClick.bind(this);
   }
 
   render() {
-    const { albums, isLoading, showAlbum, albumToShow } = this.state;
+    const { albums, isLoading, showAlbum, albumToShow } = this.props;
     let element;
     if (showAlbum) {
       element = <Album id={albumToShow} />;
@@ -97,7 +48,7 @@ const RenderAlbums = props => {
                       <img src={artwork} alt={artistName}/>
                     </div>
                     <div>
-                      <button onClick={props.handleClick} id={albumId}>View Tracklist</button>
+                      <button id={albumId}>View Tracklist</button>
                     </div>
                     <hr />
                   </div>
--- src/App.js
+++ src/App.js
@@ -2,8 +2,66 @@ import React, {Component} from 'react';
 import Albums from './Albums';
 import 'typeface-roboto';
 import Typography from '@material-ui/core/Typography';
+import axios from 'axios';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      albums: [],
+      tracks: [],
+      isLoading: true,
+      showAlbum: false,
+      errors: null, 
+      albumToShow: '',
+      playSong: false,
+      songToPlay: ''
+    }
+    // this.handleAlbums = this.handleAlbums.bind(this);
+    this.handleAlbum = this.handleAlbum(this);
+    // this.handleSong = this.handleSong(this);
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  handleAlbum(albumId) {
+    const trackEndpoint = `${albumId}&entity=song`
+    this.fetchData(trackEndpoint)
+    this.setState({
+      album: albumId,
+    });
+  }
+
+
+  fetchData(endpoint='994656&entity=album'){
+    const url = `https://cors-anywhere.herokuapp.com/https://itunes.apple.com/lookup?id=${endpoint}`;
+    axios
+      .get(`${url}`)
+      .then(response => 
+        this.mapResponse(response.data.results)
+      )
+      .then(music => {
+        music.shift()
+        this.setState({
+          albums: music,
+          isLoading: false,
+        });
+      })
+      .catch(error => this.setState({ error, isLoading: false }));
+  }
+
+  mapResponse(data) {
+    const albumArr = data.map(album => ({
+      artistName: `${album.artistName}`,
+      albumName: `${album.collectionName}`,
+      artwork: `${album.artworkUrl100}`,
+      albumId: `${album.collectionId}`,
+    }))
+    return albumArr;
+  }
+
   render() {
     return (
       <div>
@@ -11,7 +69,7 @@ export default class App extends Component {
           Led Zeppelin Compilation
         </Typography>
         <hr />
-        <Albums />
+        <Albums isLoading={this.state.isLoading} albums={this.state.albums}/>
       </div>
     );
   }
